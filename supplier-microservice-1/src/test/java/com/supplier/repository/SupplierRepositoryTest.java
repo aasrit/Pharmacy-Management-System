d62e47dcffab0e6f6826ff1e8522a71f546bc422
aasrit <115695448+aasrit@users.noreply.github.com>
2022-12-09 14:55:03 +0530

    Supplier microservice

---
+++ supplier-microservice-1/src/test/java/com/supplier/repository/SupplierRepositoryTest.java
@@ -0,0 +1,50 @@
+
+package com.supplier.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.supplier.model.Supplier;
+
+/**
+ * @author Lenovo
+ *
+ */
+@SpringBootTest
+class SupplierRepositoryTest {
+
+	/**
+	 * Test method for {@link com.supplier.repository.SupplierRepository#existsBySupplierId(java.lang.String)}.
+	 */
+	
+	/*@Autowired
+	private SupplierRepository repository;
+	
+	@Test
+	void testExistsBySupplierId() {
+		
+		List<String> drugs = new ArrayList<>();
+		drugs.add("Afrezza");
+		drugs.add("Tyvaso DPI");
+		
+		Supplier supplier = new Supplier();
+		supplier.SupplierId("8");
+		supplier.setSupplierName("MannKind Corporation");
+		supplier.setEmail("mankind@gmail.com");
+		supplier.setAvailableDrugs(drugs);
+		
+		repository.save(supplier);
+		
+		Boolean actualResult = repository.existsBySupplierId(11);
+		
+		assertThat(actualResult).isTrue();
+		
+	}*/
+
+}
